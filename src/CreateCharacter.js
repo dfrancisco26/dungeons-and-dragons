23fe08cb233fba878304242c856a443c2b0dd697
David Francisco <dfrancisco2677@gmail.com>
2022-07-20 14:27:18 -0700

    You can add stats to character sheet upon creation.

--- src/CreateCharacter.js
+++ src/CreateCharacter.js
@@ -12,6 +12,12 @@ export default function CreateCharacter() {
   const [raceInput, setRaceInput] = useState('dwarf');
   const [raceQuery, setRaceQuery] = useState('');
   const [name, setName] = useState('');
+  const [strength, setStrength] = useState(1);
+  const [dexterity, setDexterity] = useState(1);
+  const [constitution, setConstitution] = useState(1);
+  const [intelligence, setIntelligence] = useState(1);
+  const [wisdom, setWisdom] = useState(1);
+  const [charisma, setCharisma] = useState(1);
 
 
   // const race = dRace.find(singlerace => singlerace.name === raceInput); (just in case we can no longer hit the API for single races/classes)
@@ -22,6 +28,12 @@ export default function CreateCharacter() {
     name: name,
     class: classInput,
     race: raceInput,
+    strength: strength,
+    dexterity: dexterity,
+    constitution: constitution,
+    intelligence: intelligence,
+    wisdom: wisdom,
+    charisma: charisma
   };
 
 
@@ -51,7 +63,6 @@ export default function CreateCharacter() {
     setClassQuery(classInput);
     setRaceQuery(raceInput);
     const data = await getClass(classInput);
-
     const rdata = await getRace(raceInput);
     setDrace(rdata.results);
     setDclass(data.results); //this could be an issue
@@ -66,7 +77,7 @@ export default function CreateCharacter() {
 
 
   return (
-    <><div>
+    <div>
       <form className='createchar-form' onSubmit={handleSubmit}>
         <label>Name:  <input id='name-input' value={name} onChange = {e => setName(e.target.value)}></input></label>
         <br></br>
@@ -94,13 +105,22 @@ export default function CreateCharacter() {
             }
           </select>
         </label>
+        <label>Strength</label>
+        <input id='str' value={strength} onChange={e => setStrength (e.target.value)}></input>
+        <label>Dexterity</label>
+        <input id='dex' value={dexterity} onChange={e => setDexterity (e.target.value)}></input>
+        <label>Constitution</label>
+        <input id='con' value={constitution} onChange={e => setConstitution (e.target.value)}></input>
+        <label>Intelligence</label>
+        <input id='int' value={intelligence} onChange={e => setIntelligence (e.target.value)}></input>
+        <label>Wisdom</label>
+        <input id='wis' value={wisdom} onChange={e => setWisdom (e.target.value)}></input>
+        <label>Charisma</label>
+        <input id='cha' value={charisma} onChange={e => setCharisma (e.target.value)}></input>
         <br></br>
         <button id='submit-button'>Submit Selection</button>
       </form>
     </div>
-    <div>
-
-    </div></>
 
   );
 }
